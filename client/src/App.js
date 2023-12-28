b3e7c668c5b6605e61825084b71f8424deadd1b4
sarac721 <123259230+sarac721@users.noreply.github.com>
2023-12-27 20:35:32 -0500

    Se creo las paginas de Bienvenida

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Botones, { BtnIndividual } from './components/Bienvenido/BtnBienvenida';
+import Navegar from './components/Navbar/Navbar';
+import BienvenidaAdmi from './pages/BienvenidaAdmi';
+import BienvenidaTrabajador from './pages/BienvenidaTrabajador';
+import BienvenidaVendedor from './pages/BienvenidaVendedor';
 
 function App() {
   return (
     <div>
-      
-       
+    <BienvenidaVendedor/>
+  
 
     </div>
   );
